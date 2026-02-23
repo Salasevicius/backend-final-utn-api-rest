31ff809b5d6df36cd3d650ec96d935524880551f
Salasevicius <salasevicius1@gmail.com>
2026-02-23 19:50:33 -0300

    Autenticación y seguridad. User model y IPayload interface

---
+++ src/models/user.model.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true }
+},
+  { versionKey: false }
+)
+
+const User = mongoose.model("User", UserSchema)
+
+export { User }
